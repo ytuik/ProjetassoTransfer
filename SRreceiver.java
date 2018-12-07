5a5d32c48c9f6330a466ebf3a51f96bd2f0a1837
Jose Gabriel Silva Pereira <jgsp2@WINDOWS.CIN.UFPE.BR>
2018-12-07 20:21:42 -0200

    comentarois adicionados

--- SRreceiver.java
+++ SRreceiver.java
@@ -6,32 +6,63 @@ public class SRreceiver{
     public int qntrecebidos;//numero de pacotes confirmados
     public byte [] arquivo;//array de bytes compondo o arquivo
     public int []state;//0-enviaveis mas nao enviados   1-confirmado 2-esperando ack
-    public int mod;
+	public int mod;//q deve ser (o dobro do tamanho da janela) +1
+	public byte [] buffer;//pacotes bufferizados serao colocados aqui
+	public List<Byte> myBytes;
 
-
-    public void run(){
-    	  i = 0;
-    	  j = tamanhojanela-1;
-    	  for(int k = i; k<=j;k++){
-    	  	state[k] = 1;
-    	  }
-        while(qntrecebidos<numerodebytes){
-           		 //receber arquivo e indice
-           		 if(state[index] == 0){
-           		 	//send Ack
-           		 }else{
-		             state[index]=0;//confirma pacote
-		             //send Ack
-		             //Salvar Buffer;//array de bytes (index)
-		             qntrecebidos++;
-		             while(state[i]==0){
-		                 //Inserir no vector
-		                 i=(i+1)%mod;
-		                 state[(j+1)%mod] = 1;
-		                 j=(j+1)%mod;
-		             }
-           		 }
-		}
+	public SRreceiver(int porta, int tamanhojanela, int numerodebytes){
+		this.qntrecebidos=0;
+		this.tamanhojanela=tamanhojanela;
+		this.porta=porta;
+		this.state= new int[tamanhojanela];
+		this.buffer = new byte[tamanhojanela];
+		this.i=0;
+		this.j=tamanhojanela-1;
+		this.mod = tamanhojanela*2 +1;
+		List<Byte> myBytes = new ArrayList<Byte>();
 	}
 
+    public void run(){
+    	for(int k = i; k<=j;k++){
+			state[k] = 1;//desconfirma todos os indices na 1a janela
+			//todos os outros estão com 0, significando que estão confirmados
+    	}
+        while(qntrecebidos<numerodebytes){
+				/**
+				 * RECEBER BYTE E SEU INDICE NA JANELA AQUI
+				 * VIA PORTA DO SERVIDOR
+				 */
+        	if(state[index] == 0){
+           	 	/**
+					 * ENVIAR ACK (inteiro igual a "index" pro cliente)
+					 * POIS O STATE=0 SIGNIFICA Q ELE JÁ FOI RECEBIDO E CONFIRMADO
+					 * OU QUE ELE ESTÁ FORA DA JANELA ATUAL 
+				*/
+           	}else{
+		        state[index]=0;//confirma pacote
+		        //ENVIAR ACK (inteiro igual a "index" pro cliente)
+
+				//Salvar Buffer;//array de bytes (index)
+				//buffer[index]="BYTE RECEBIDO AQUI";
+
+				qntrecebidos++;//conta mais um recebido
+				
+		        while(state[i]==0){
+					
+					//ENQUANTO O 1o DA JANELA ESTIVER CONFIRMADO, COLOCAR ELE NA LISTA DE BYTES ORDENADOS
+			
+					//HERE//myBytes.add(byte recebido aqui)
+			
+					//ESSA LISTA DE BYTES ORDENADOS SERÁ USADOS DEPOIS PRA JUNTAR OS BYTES NA ORDEM E TRANSFORMA-LOS NO ZIP
+					
+
+					//aqui abaixo a janela do servidor anda uma unidade, atualizando o estado do elemento que acaba de entrar para "não confirmado"
+					i=(i+1)%mod;
+		            state[(j+1)%mod] = 1;
+		            j=(j+1)%mod;
+		        }
+           	}
+		}
+		//TALVEZ USAR A LISTA PARA RECRIAR O ZIP AQUI (fim do run)
+	}
 }
--- SRsender.java
+++ SRsender.java
@@ -1,22 +1,22 @@
 import java.util.Random;
-public class SRsender{//talvez tenha q troar os public por static
+public class SRsender{//talvez tenha q trCoar os public por static
     public int numerodebytes;//a serem enviados
     public int porta; //porta udp para a qual enviar
     public int tamanhojanela;//tamanho da janela q deve ser (tamanho da seq)/2
     public int qntenviados;//numero de pacotes com acks recebidos
     public byte [] arquivo;//array de bytes compondo o arquivo
-    public int []state;//0-enviaveis mas nao enviados   1-confirmado 2-esperando ack
+    public int []state;//0-enviaveis mas nao enviados   1-confirmado    2-esperando ack
     public long timer;
-    public int i, j;
-    public int probability;
-    public int mod;
+    public int i, j;//limites da janela
+    public int probability;//definirá a probabilidade de descartar
+    public int mod;//q deve ser (o dobro do tamanho da janela) +1
     public SRsender(int porta, int tamanhojanela, byte []arquivo, long timeout,int  probability){
         this.porta=porta;
         this.tamanhojanela=tamanhojanela;
         this.qntenviados=0;
         this.timeout = timeout;
-        this.arquivo = arquivo;
         this.numerodebytes=arquivo.length;
+        this.arquivo = arquivo;
         this.state = new int[tamjanela];
         for(int k=0;k<tamanhojanela;k++)state[k]=0;
         this.i=0;
@@ -24,21 +24,29 @@ public class SRsender{//talvez tenha q troar os public por static
         this.probability=probability;
         this.mod=tamanhojanela*2 +1;
     }
-public static boolean Descarta(int x){//x é o número definido pelo usuário
+    public static boolean Descarta(int x){//x é o número definido pelo usuário de 0 a 100
 	      Random gerador = new Random();
     		if(x<gerador.nextInt(100)) return true;
     		else return false;
-    	}
+    }
+    //na janela mas não enviado == 0
+    //Já confirmado == 1
+    //Enviado mas não confirmado == 2
     class Send extends Thread{
         public void run(){
             timer = System.nanoTime();
             int aux = System.nanoTime();
             while(qntenviados<numerodebytes){
-                if(timer-aux>=timeout){
-                    for(int k=i;k<=j;k++){
-                        if(state[k]==0 || state[k]==2){
+                if(timer-aux>=timeout){//sempre q der timeout
+                    for(int k=i;k<=j;k++){//percorre a janela
+                        if(state[k]==0 || state[k]==2){//se na janela e nao enviado ou na janela mas nao confirmado
+                           
+                           
                             //envia arquivo[k] e indice k%mod porta
-                            state[k]=2;//marca como enviado
+                            //ENVIAR BYTE EM arquivo[K] e indice k%mod AQUI //PROVAVELMENTE SERÁ NECESSÁRIO FAZER UM OBJETO COM O BYTE E O INDICE
+                           
+                           
+                            state[k]=2;//marca como enviado mas nao confirmado
                         }
                     }
                     timer = System.nanoTime();//reseta timer
@@ -46,28 +54,29 @@ public static boolean Descarta(int x){//x é o número definido pelo usuário
             }
         }
     }
-//Utilizado mas não enviado == 0
-//Já confirmado == 1
-//Enviado mas não confirmado == 2
-class Receive extends Thread{
-    int index;//indice recebido do servidor
-   
-    public void run(){
-        while(qntenviados<numerodebytes){
-            //receber index aqui
-            
-            if(state[index]==2 && !Descarta(probability)){
-                state[index]=1;//confirma pacote
-                qntenviados++;//conta mais um confirmado
-                while(state[i]==1){
-                    state[i] = 0;
-                    i=(i+1)%mod;
-                    j=(j+1)%mod;
-                }
+    //na janela mas não enviado == 0
+    //Já confirmado == 1
+    //Enviado mas não confirmado == 2
+    class Receive extends Thread{
+        int index;//indice a ser recebido do servidor
+    
+        public void run(){
+            while(qntenviados<numerodebytes){
+                
+                //RECEBER INDICE (que será o ack) AQUI
+                
+                if(state[index]==2 && !Descarta(probability)){//se nao foi confirmado e nao descartar o ack
+                    state[index]=1;//confirma pacote
+                    qntenviados++;//conta mais um confirmado
+                    while(state[i]==1){//enquanto o 1o da janela tiver sido confirmado
+                        state[i] = 0;//desconfirma o 1o da janela
+                        i=(i+1);//anda janela
+                        j=(j+1);
+                    }
 
+                }
             }
-		}
-	}
+        }
 }
 
 //0 1 2 3 4 0 1 2 3 4
