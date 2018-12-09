319cf66eef49b44cfdbea51e5d9710c142c2ff08
Leao Liu Masur <llm2@cin.ufpe.br>
2018-12-09 17:20:08 -0300

    Cliente e Servidor UDP para arquivos.

---
+++ ClienteServidorUDP/ClienteUDP.java
@@ -0,0 +1,41 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.File;
+
+
+public class ClienteUDP {
+    public static void main(String[] args) throws IOException {
+        DatagramSocket socket = new DatagramSocket();
+        InetAddress IP = InetAddress.getByName("localhost");
+        byte[] dados = new byte[200];
+        
+        File arquivo = new File("C:\\Users\\Lion\\Desktop\\Opa.zip"); // Carrega o arquivo a ser enviado
+        FileInputStream entradaArquivo = new FileInputStream(arquivo);
+
+        DatagramPacket pacote = new DatagramPacket(dados, dados.length, IP, 5000); // Cria o pacote que vai mandar o array dados[]
+
+        int tamanho = entradaArquivo.read(dados); // Retorna o tamanho dos dados.
+        // Deve dar pra manipular isso aqui direito pra mandar as sequencias do SR
+        while(tamanho != -1) {
+            System.out.println("Enviando parte " + tamanho);
+            socket.send(pacote); 
+            tamanho = entradaArquivo.read(dados); // Depois que o dado é enviado, o tamanho fica -1, não sei pq
+        }
+        dados = "fyn".getBytes();
+
+        pacote = new DatagramPacket(dados, dados.length, IP, 5000);
+        System.out.println("Arquivo enviado");
+        socket.send(pacote);
+        
+        socket.close();
+        entradaArquivo.close();
+        
+    }
+}
---
+++ ClienteServidorUDP/ServidorUDP.java
@@ -0,0 +1,34 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.File;
+
+public class ServidorUDP {
+    public static void main(String[] args) throws IOException {
+
+        DatagramSocket socket = new DatagramSocket(5000);
+        File arquivo = new File("C:\\Users\\Lion\\Desktop\\Epa.zip"); // Cria o arquivo que vai receber os bytes do cliente
+        FileOutputStream saidaArquivo = new FileOutputStream(arquivo); 
+        byte[] receiveData = new byte[200];
+        DatagramPacket pacoteRecebido = new DatagramPacket(receiveData,receiveData.length);
+
+        while (true) {
+            socket.receive(pacoteRecebido);
+            String terminou = new String(pacoteRecebido.getData(), 0, pacoteRecebido.getLength()); // Quando receber o pacote com a String fyn, significa que é o ultimo pacote.
+            if (terminou.equals("fyn")) {
+                System.out.println("Documentos recebidos");
+                break;
+            }
+            saidaArquivo.write(pacoteRecebido.getData(), 0, pacoteRecebido.getLength()); // Escreve no arquivo
+
+            saidaArquivo.flush(); // Garante que os bytes passem para o SO.
+        }
+        saidaArquivo.close();
+        socket.close();
+    }
+}
