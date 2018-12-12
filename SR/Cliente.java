30e6baa3564b6b2331d6c938b3ae5ae420393d6b
Gabriel Teixeira Soares de Almeida <gtsa@WINDOWS.CIN.UFPE.BR>
2018-12-12 17:59:26 -0300

    Mudança para o servidor não ter input

--- SR/Cliente.java
+++ SR/Cliente.java
@@ -6,17 +6,17 @@ import java.util.Scanner;
 
 public class Cliente {
     public static void main(String[] args) throws Exception {
-
+        DatagramSocket clientSocket = new DatagramSocket();
         Scanner s = new Scanner(System.in);
         System.out.println("Deseja escolher o tamanho da janela? [1/0]");// o padrão será 10
-        int tamanho;
+        String tamanho;
         boolean escolha;
         while (true) {
-            tamanho = s.nextInt();
-            if (tamanho == 1) {
+            tamanho = s.nextLine();
+            if (tamanho.equals("1")) {
                 escolha = true;
                 break;
-            } else if (tamanho == 0) {
+            } else if (tamanho.equals("0")) {
                 escolha = false;
                 break;
             } else {
@@ -25,7 +25,7 @@ public class Cliente {
         }
         if (escolha) {
             System.out.println("Tamanho da janela:");
-            tamanho = s.nextInt();
+            tamanho = s.nextLine();
         }
 
         System.out.println("Probabilidade de descarte sem utilizar % :");
@@ -38,16 +38,27 @@ public class Cliente {
                 System.out.println("Valor inválido, por favor, digite um número de 0 a 99.");
             }
         }
-
-        System.out.println("Digite o nome do arquivo a ser solicitado :");
+        s.nextLine();
+        System.out.println("Digite o nome do arquivo a ser criado :");
         String fileName = s.nextLine();
 
         int port = 8001;
         DatagramSocket socket = new DatagramSocket(port);
         ClienteSR receiver;
         if (escolha) {
-            receiver = new ClienteSR(socket, fileName, tamanho, prob);
+        	InetAddress IPServer = InetAddress.getByName("localhost");
+        	String aux = fileName + "#" + tamanho;
+        	byte[] sendData = aux.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length,IPServer, 5000);
+            clientSocket.send(sendPacket);
+            int winSize = Integer.parseInt(tamanho);
+            receiver = new ClienteSR(socket, fileName,winSize, prob);
         } else {
+        	InetAddress IPServer = InetAddress.getByName("localhost");
+            String aux = fileName + "#10";
+            byte[] sendData = aux.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length,IPServer, 5000);
+            clientSocket.send(sendPacket);
             receiver = new ClienteSR(socket, fileName, prob);
         }
         receiver.start();
--- SR/Servidor.java
+++ SR/Servidor.java
@@ -1,24 +1,33 @@
 import java.io.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.util.Scanner;
 
 public class Servidor {
     public static void main(String[] args) throws Exception {
 
         Scanner s = new Scanner(System.in);
-        System.out.println("Digite o tamanho da janela:");
-        int j = s.nextInt();
-        System.out.println("Digite o nome do arquivo que deseja mandar:");
-        String fileName = s.nextLine();
+        DatagramSocket serverSocket =new DatagramSocket(5000);
+        byte[] receiveData= new byte[1000];
+        InetAddress clientIP;
+        DatagramPacket receivePacket=new DatagramPacket(receiveData,receiveData.length);
+        serverSocket.receive(receivePacket);
+        String fiilexWin = new String(receiveData, "UTF-8");
+        fiilexWin.trim();
+        String[] divide = fiilexWin.split("#");
+        System.out.println("Tamanho da janela: "+divide[1]);
+        System.out.println("Nome do arquivo solicitado: "+ divide[0]);
         
         int port = 1235;
 
         // Checa arquivo
-        File f = new File(fileName);
+        File f = new File(divide[1]);
         if (!f.exists() || !f.canRead()) {
             throw new Exception("Arquivo inexistente");
         }
 
-        ServidorSR sender = new ServidorSR(fileName, port, j);
+        ServidorSR sender = new ServidorSR(divide[0], port, Integer.parseInt(divide[1]));
         sender.start();
     }
 }
