8ea7d871f78eabc1e58aadc581792e3ce6a887a3
Gabriel Teixeira Soares de Almeida <gtsa@WINDOWS.CIN.UFPE.BR>
2018-12-12 23:11:27 -0200

    Comentários corrigidos

--- SR/Servidor.java
+++ SR/Servidor.java
@@ -8,7 +8,7 @@ public class Servidor {
     public static void main(String[] args) throws Exception {
 
         Scanner s = new Scanner(System.in);
-        System.out.println("Servidor iniciado - Aguardando conexão.");
+        System.out.println("Servidor iniciado - Aguardando conexao.");
         DatagramSocket serverSocket =new DatagramSocket(5000);
         byte[] receiveData= new byte[1000];
         InetAddress clientIP;
@@ -16,20 +16,20 @@ public class Servidor {
         serverSocket.receive(receivePacket);
         String filexWin = new String(receiveData, "UTF-8");//Pacote enviado pelo cliente solicitando certo arquivo
         filexWin = filexWin.trim();
-        System.out.println("Conexão estabelecida");
+        System.out.println("Conexao estabelecida");
         System.out.println("IP: "+ receivePacket.getAddress());// IP do cliente
         System.out.println("Porta: "+ receivePacket.getPort());// Porta do cliente
         String[] divide = filexWin.split("#");//caractere especial que foi decidido
         System.out.println("Tamanho da janela: "+divide[1]);// tamanho da janela solicitada pelo cliente
         System.out.println("Nome do arquivo solicitado: "+ divide[0]);
-        int port = 1235;//Porta de conexão, caso dê erro nesta posta, mudar também em ClienteSR
+        int port = 1235;//Porta de conexao, caso de erro nesta posta, mudar tambem em ClienteSR
 
         // Checa arquivo
         divide[0] = divide[0].trim();
-        String diret = "C:\\Users\\gtsa\\Desktop\\" + divide[0];//Mude esse diretório como se fosse a pasta padrão onde estará
+        String diret = "C:\\Users\\gtsa\\Desktop\\" + divide[0];//Mude esse diretorio como se fosse a pasta padrao onde estara
         //os arquivos
-        File f = new File(diret);//File com o diretório anterior
-        if (!f.exists() || !f.canRead()) { // em caso de arquivo não estar na pasta
+        File f = new File(diret);//File com o diretorio anterior
+        if (!f.exists() || !f.canRead()) { // em caso de arquivo nao estar na pasta
             throw new Exception("Arquivo inexistente");
         }
         int janel = Integer.parseInt(divide[1]);
